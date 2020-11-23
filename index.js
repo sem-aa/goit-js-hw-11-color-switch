4ffb6c74910034cf13ea38e690693d4352f0ad06
sem-aa <66129330+sem-aa@users.noreply.github.com>
2020-11-24 00:12:07 +0200

    Выполняет ДЗ 11 часть 1

---
+++ index.js
@@ -0,0 +1,35 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log(randomIntegerFromInterval(0, 5));
+
+const body = document.querySelector(".body");
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const PROMT_DELAY = 1000;
+
+startBtn.addEventListener("click", clickStart);
+
+function clickStart() {
+  console.log("Start");
+  const intervalId = setInterval(() => {
+    console.log("intrerval");
+    body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, PROMT_DELAY);
+  startBtn.disabled = true;
+
+  stopBtn.addEventListener("click", clickStop);
+  function clickStop() {
+    console.log("Stop");
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+  }
+}
